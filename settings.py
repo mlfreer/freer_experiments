6e364f3be74b43766b9da416db1c755c1eec0de7
Mikhail Freer <1991F@inbox.ru>
2024-02-20 09:17:27 +0000

    changing config

--- settings.py
+++ settings.py
@@ -96,7 +96,7 @@ DEBUG = True
 # e.g. self.session.config['participation_fee']
 
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=1.00, participation_fee=6.00, doc=""
+    real_world_currency_per_point=1.00, participation_fee=5.00, doc=""
 )
 
 ROOMS = [
