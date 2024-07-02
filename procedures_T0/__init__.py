cd0f56088da0f76bad92f8149f40151fbdcd8f4a
mlfreer <1991F@inbox.ru>
2024-07-02 17:54:53 +0100

    prototype

--- procedures_T0/__init__.py
+++ procedures_T0/__init__.py
@@ -9,7 +9,9 @@ Your app description
 class C(BaseConstants):
     NAME_IN_URL = 'procedures_T0'
     PLAYERS_PER_GROUP = None
-    NUM_ROUNDS = 1
+    NUM_ROUNDS = 5
+
+    BUDGET_SIZE = [6, 6, 6, 6, 6]
 
 
 class Subsession(BaseSubsession):
@@ -21,12 +23,23 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    choice = models.IntegerField(default = -1)
 
 
 # PAGES
 class Decision(Page):
-    pass
+    form_model = 'player'
+    form_fields = ['choice']
+
+    @staticmethod
+    def vars_for_template(player):
+
+        return dict(
+            total_rounds = C.NUM_ROUNDS,
+            round = player.subsession.round_number,
+            budget = [0,1,2,3,4,5],
+            budget_size = 6,
+            )
 
 
 class ResultsWaitPage(WaitPage):
@@ -38,6 +51,6 @@ class Results(Page):
 
 
 page_sequence = [Decision, 
-                ResultsWaitPage, 
-                Results
+#                ResultsWaitPage, 
+#                Results
                 ]
