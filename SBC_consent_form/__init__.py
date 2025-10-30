c4969f04666ce452f02a0d0615dc039c80ce4a4b
Mikhail Freer <mikhaillfreer@gmail.com>
2025-10-30 14:35:36 +0000

    including quiz logic

--- SBC_consent_form/__init__.py
+++ SBC_consent_form/__init__.py
@@ -14,6 +14,9 @@ class C(BaseConstants):
     NUM_ROUNDS = 1
     ENDOWMENT = 15
 
+    # QUIZ ANSWERS:
+    QUIZ_ANSWERS = [2,3,0]
+
 
 class Subsession(BaseSubsession):
     pass
@@ -25,6 +28,15 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     consent = models.BooleanField()
+
+    # variables for the quiz answers:
+    q1 = models.IntegerField()
+    q2 = models.IntegerField()
+    q3 = models.IntegerField()
+    # model to count the quiz attempts
+    quiz_attempts = models.IntegerField(initial = 0)
+    return_study = models.IntegerField(initial = 0)
+    # Prolific rule: 3 fails => return the study
 #-----------------------------------------------------------------------------
 
 
@@ -53,19 +65,34 @@ class ConsentForm(Page):
 class ReturnStudy(Page):
     template_name = "_static/global/ReturnStudy.html"
     def is_displayed(player: Player):
-        return player.consent==False
+        return (player.consent==False) or (player.return_study == True)
 
 class Instructions(Page):
     pass
 
 class Quiz(Page):
-    pass
+    form_model = 'player'
+    form_fields = ['q1','q2','q3']
+
+    @staticmethod
+    def is_displayed(player):
+        return (player.round_number == 1) and (player.return_study == False)
+
+    def error_message(player, value):
+        if ((value['q1']!=C.QUIZ_ANSWERS[0]) or (value['q2']!=C.QUIZ_ANSWERS[1]) or (value['q3']!=C.QUIZ_ANSWERS[2])) and (player.return_study == 0):
+            result = 'Wrong answer! Try again!'
+            player.quiz_attempts = player.quiz_attempts + 1
+            if player.quiz_attempts >= 2:
+                player.return_study = 1
+            return result
+
 #-----------------------------------------------------------------------------
 
 page_sequence = [ConsentForm,  
                 ReturnStudy, 
                 Instructions,
-                Quiz]
+                Quiz,
+                ReturnStudy]
 
 
 
