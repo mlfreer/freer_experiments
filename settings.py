d079c3aa6bc9c44a3fac1c08a8e7126477570781
Mikhail Freer <mikhaillfreer@gmail.com>
2026-07-23 11:43:38 +0100

    changing the settings

--- settings.py
+++ settings.py
@@ -142,7 +142,7 @@ DEBUG = True
 
 SESSION_CONFIG_DEFAULTS = dict(
     real_world_currency_per_point=1.00,
-    participation_fee=3.00,
+    participation_fee=6.00,
     min_pay = 1.00,
     max_pay = 15.00,
     doc=""
@@ -197,7 +197,7 @@ participation_fee = 5
 
 # ISO-639 code
 # for example: de, fr, ja, ko, zh-hans
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'de'
 
 # e.g. EUR, GBP, CNY, JPY
 REAL_WORLD_CURRENCY_CODE = 'EUR'
